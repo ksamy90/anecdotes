04f7c9f87ba8fb13b0df0f91f949b1c034dbd731
samson <samson.omondi1@ibm.com>
2023-04-11 17:13:05 +0300

    add votes for quotes

--- src/App.js
+++ src/App.js
@@ -13,14 +13,26 @@ const App = () => {
   ];
 
   const [selected, setSelected] = useState(0);
+  let newArray = Array(anecdotes.length).fill(0);
+  const [votes, setVotes] = useState(newArray);
 
   const handleClick = () => {
     setSelected(Math.floor(Math.random() * anecdotes.length));
   };
+  const handleVote = () => {
+    if (anecdotes[selected]) {
+      let points = [...votes];
+      points[selected] += 1;
+      setVotes(points);
+    }
+  };
 
   return (
     <div>
-      <div>{anecdotes[selected]}</div>
+      <div>
+        {anecdotes[selected]} has {votes[selected]} votes
+      </div>
+      <button onClick={handleVote}>vote</button>
       <button onClick={handleClick}>next anecdote</button>
     </div>
   );
